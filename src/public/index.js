97deb5ff6e35d6b39b18bc535d359f1c091231f2
Andres Lubo <andresl@sempertex.local>
2022-12-01 17:19:49 -0500

    xx00q12pas

--- src/public/index.js
+++ src/public/index.js
@@ -1,39 +1,37 @@
-const socket = io()
-
+const socket = io();
 
 //* DOM elements
-const message = document.getElementById('message')
-const username = document.getElementById('username')
-const btn = document.getElementById('send')
-const output = document.getElementById('output')
-const actions = document.getElementById('actions')
+const message = document.getElementById("message");
+const username = document.getElementById("username");
+const btn = document.getElementById("send");
+const output = document.getElementById("output");
+const actions = document.getElementById("actions");
 
-btn.addEventListener('click', (e) => {
-    socket.emit('client:chatmessage', {
-        username: username.value,
-        message: message.value
-    })
-    
-})
+btn.addEventListener("click", (e) => {
+  socket.emit("client:chatmessage", {
+    username: username.value,
+    message: message.value,
+  });
+});
 
-message.addEventListener('keypress', (e) => {
-    socket.emit('client:chattyping', username.value)
-})
+message.addEventListener("keypress", (e) => {
+  socket.emit("client:chattyping", username.value);
+});
 
-socket.on('server:newmessage', data => {
-    actions.innerHTML = ''
-    output.innerHTML += `
+socket.on("server:newmessage", (data) => {
+  actions.innerHTML = "";
+  output.innerHTML += `
         <p>
             <strong>${data.username}</strong> : ${data.message}
         </p>
-    `
-    console.log(data);
-})
+    `;
+  console.log(data);
+});
 
-socket.on('server:chattyping', (data) => {
-    actions.innerHTML = `
+socket.on("server:chattyping", (data) => {
+  actions.innerHTML = `
         <p>
             <en>${data} is typing a message...</en>
         </p>
-    `
-})
+    `;
+});
